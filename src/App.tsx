782e01916acf466fa7170bcdfd7daf079baaf1b1
Bruno Braga <brunobragaw8t@gmail.com>
2026-06-11 12:13:48 +0100

    chore: add shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <div>
       <h1 className="font-bold text-center py-6">Task Bar Hero Runes Inspector</h1>
+      <Button>Dummy button</Button>
     </div>
   );
 }
